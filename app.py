e8db3dfbdffb9d0aca9ac247a27d6ba6ec5d29ed
Yiwen Deng <1800013732@pku.edu.cn>
2026-04-15 23:35:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
-st.markdown("""
-<style>
-html, body, [class*="css"]  {
-    font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif !important;
-}
-</style>
-""", unsafe_allow_html=True)
+#st.markdown("""
+#<style>
+#* {
+#    font-family: monospace !important;
+#}
+#</style>
+#""", unsafe_allow_html=True)
 
 st.set_page_config(layout="wide")
 st.title("🧠 Social Curiosity Agent (Tunable)")
